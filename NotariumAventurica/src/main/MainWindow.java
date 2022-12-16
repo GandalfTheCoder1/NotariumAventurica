c7a659e9652387578ab92c143d723cbdda70ba2b
GandalfTheCoder1 <39965725+GandalfTheCoder1@users.noreply.github.com>
2022-12-17 00:09:16 +0100

    Frame added

--- NotariumAventurica/src/main/MainWindow.java
+++ NotariumAventurica/src/main/MainWindow.java
@@ -3,6 +3,8 @@ import java.awt.*;
 
 public class MainWindow extends Frame implements WindowOperations{
 	public MainWindow() {
-		
+		//setLayout(new FlowLayout());
+		new Frame();
+		setVisible(true);
 	}
 }
